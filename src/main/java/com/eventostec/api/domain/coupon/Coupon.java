23223e1f970c8652d0c917ca46ac62cf39e97caa
Ícaro Fonseca <icaro_mota10@hotmail.com>
2026-05-05 15:32:21 -0300

    feat: Add new entity.

--- src/main/java/com/eventostec/api/domain/coupon/Coupon.java
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -10,12 +10,12 @@ import lombok.Setter;
 import java.util.Date;
 import java.util.UUID;
 
-@Entity
-@Table(name = "coupon")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity                    //define a classe como uma entidade do banco de dados
+@Table(name = "coupon")    //vincula a tabela coupon
+@Getter                    //Gera os getters automaticamente
+@Setter                    //gera os setters automaticamente
+@NoArgsConstructor         //inicializa um construtor vazio
+@AllArgsConstructor        //inicializa um contrutor com todos os argumentos da classe
 public class Coupon {
     @Id
     @GeneratedValue
@@ -26,6 +26,6 @@ public class Coupon {
     private Integer discount;
 
     @ManyToOne
-    @JoinColumn(name = "event_id")
-    private Event event;
+    @JoinColumn(name = "event_id")  //vincula a chave estrangeira
+    private Event event;            //FK
 }
